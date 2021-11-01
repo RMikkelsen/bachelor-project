da439b5c261c80e86945a6adfe259f1cddcbf525
Ruth Mikkelsen <rmm@impact.dk>
2021-11-01 10:08:35 +0100

    cleaning up files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,19 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ruth's Bachelor Project </h1>
+      <h3>Learning Onboarding Portal</h3>
     </div>
   );
 }
