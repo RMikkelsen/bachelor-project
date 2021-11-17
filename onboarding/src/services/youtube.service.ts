e35d2ba0297163556826d10fb462b96e15189318
Ruth Mikkelsen <rmm@impact.dk>
2021-11-17 15:44:58 +0100

    added in some axios to check

--- onboarding/src/services/youtube.service.ts
+++ onboarding/src/services/youtube.service.ts
@@ -1,3 +1,22 @@
+// import axios from "axios";
+
+// export default async function searchYouTube(q: string | number | boolean) {
+//   const config = {
+//     headers: {
+//       "x-rapidapi-host": "youtube-search-results.p.rapidapi.com",
+//       "x-rapidapi-key": "5712da3fc3msh68ecf2ac4b9903ep1c5497jsn27fb0091085a",
+//     },
+//   };
+//   axios
+//     .get(
+//       "https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + q,
+//       config
+//     )
+//     .then((response) => {
+//       console.log(response.data);
+//     });
+// }
+
 export default async function searchYouTube(q: string | number | boolean) {
   //   q = encodeURIComponent(q);
   const response = await fetch(
